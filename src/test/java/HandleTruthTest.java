438858d0d7decb9cc8caf12b5c3a3909b41c1908
Jacrys3 <ryse0528@gmail.com>
2020-11-21 15:16:25 -0800

    formatting wordcount to be able to return a treemap

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,9 +1,18 @@
 import org.junit.Test;
 
+import java.util.Set;
+import java.util.TreeMap;
+
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
         //starting work
         HandleTruth.wordCount();
+    }
+
+    @Test void realTest(){
+        TreeMap<Integer, Set<String>> actual = HandleTruth.wordCount();
+        TreeMap<Integer, Set<String>> expected;
+        assert(actual, expected);
     }
 }
