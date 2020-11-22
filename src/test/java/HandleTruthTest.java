cf8ece22ede64509b0f9778a0b250e8a8888ae01
Jacrys3 <ryse0528@gmail.com>
2020-11-21 16:04:21 -0800

    all cases done, going to check for bugs now

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -26,7 +26,7 @@ public class HandleTruthTest {
                 int key = (int) keys.poll();
                 assertEquals(actual.get(key), expected.get(key));
             }
-
+            expected = new TreeMap<>();
         }
     }
 
@@ -36,26 +36,83 @@ public class HandleTruthTest {
         switch (num){
             case 1:
                 msg = "hi how are you i am great thanks hi again are you good dude what is up dude how is everything what is your name oh hi";
+                set = new HashSet<>();
+                set.add("hi");
+                set.add("is");
+                expected.put(3, set);
+                set = new HashSet<>();
+                set.add("how");
+                set.add("what");
+                set.add("are");
+                set.add("dude");
+                set.add("you");
+                expected.put(2, set);
+                set = new HashSet<>();
+                set.add("thanks");
+                set.add("again");
+                set.add("name");
+                set.add("i");
+                set.add("oh");
+                set.add("your");
+                set.add("up");
+                set.add("am");
+                set.add("everything");
+                set.add("great");
+                set.add("good");
+                expected.put(1, set);
                 break;
             case 2:
                 msg = "hey low hey high hey slow hey bye sigh nevermind bye";
+                set = new HashSet<>();
+                set.add("hey");
+                expected.put(4, set);
+                set = new HashSet<>();
+                set.add("bye");
+                expected.put(2, set);
+                set = new HashSet<>();
+                set.add("high");
+                set.add("sigh");
+                set.add("slow");
+                set.add("low");
+                set.add("nevermind");
+                expected.put(1, set);
                 break;
             case 3:
                 msg = "a dog went to a park and a different dog followed a little dog was there and a big dog was there too";
+                set = new HashSet<>();
+                set.add("a");
+                expected.put(5, set);
+                set = new HashSet<>();
+                set.add("dog");
+                expected.put(4, set);
+                set = new HashSet<>();
+                set.add("and");
+                set.add("there");
+                set.add("was");
+                expected.put(2, set);
+                set = new HashSet<>();
+                set.add("big");
+                set.add("went");
+                set.add("too");
+                set.add("to");
+                set.add("different");
+                set.add("followed");
+                set.add("park");
+                set.add("little");
+                expected.put(1, set);
                 break;
             case 4:
                 msg = "a a a a a a a a a a a a hi hi hi hi hi no no no right right";
-                expected.clear();
-                set.clear();
+                set = new HashSet<>();
                 set.add("a");
                 expected.put(12,set);
-                set.clear();
+                set = new HashSet<>();
                 set.add("hi");
                 expected.put(5,set);
-                set.clear();
+                set = new HashSet<>();
                 set.add("no");
                 expected.put(3,set);
-                set.clear();
+                set = new HashSet<>();
                 set.add("right");
                 expected.put(2,set);
                 break;
